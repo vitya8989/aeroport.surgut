46f86bd7cf5ceff167d59af0961eacfa441efa49
viktor <komarov.v@triadasite.ru>
2023-03-28 10:30:46 +0300

    3-5

--- Aeroport/js/script.js
+++ Aeroport/js/script.js
@@ -267,6 +267,40 @@ if (flightAccordionHeads.length > 0) {
         }
     });
 };
+// Слайдер "Об аэропорте"
+
+const mainAboutSlider = document.querySelector('.js_main_about_slider');
+
+if (mainAboutSlider) {
+    new Swiper('.js_main_about_slider', {
+        speed: 300,
+        spaceBetween: 15,
+        slidesPerView: 'auto',
+        pagination: {
+            el: '.main_about__slider_pagination',
+            type: 'bullets',
+            clickable: true
+        },
+        navigation: {
+            nextEl: '.main_about__slider_next',
+            prevEl: '.main_about__slider_prev',
+        },
+        breakpoints: {
+            768: {
+                spaceBetween: 32,
+                slidesPerView: 2,
+            },
+            1100: {
+                spaceBetween: 32,
+                slidesPerView: 3,
+            },
+            1500: {
+                spaceBetween: 32,
+                slidesPerView: 4,
+            }
+        }
+    });
+};
 
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 @@include('../components/header/header.js');
 @@include('../components/main/main_top/main_top.js');
 @@include('../components/main/main_online/main_online.js');
+@@include('../components/main/main_about/main_about.js');
 
 
 
